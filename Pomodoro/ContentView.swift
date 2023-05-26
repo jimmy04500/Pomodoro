4fdd0c65c0fece0740eb924412dcd06b45cfdfd0
edwardkim12 <edward.kim@sibelhealth.com>
2023-05-26 17:40:50 -0500

    Initial pomodoro project with logic

--- Pomodoro/ContentView.swift
+++ Pomodoro/ContentView.swift
@@ -8,12 +8,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @StateObject var pomodoroViewModel: PomodoroViewModel
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            if pomodoroViewModel.currentStep == .PomodoroNotStarted {
+                Text("Start Pomodoro \(pomodoroViewModel.completedPomodoros+1)/4")
+                Button(action: pomodoroViewModel.startPomodoro) {
+                    Text("Start")
+                }
+            } else if pomodoroViewModel.currentStep == .PomodoroInProgress {
+                Text("Work Hard! \(pomodoroViewModel.displayTimeRemaining)")
+            } else if pomodoroViewModel.currentStep == .ShortBreakNotStarted {
+                Text("Start Short Break")
+                Button(action: pomodoroViewModel.startShortBreak) {
+                    Text("Start")
+                }
+            } else if pomodoroViewModel.currentStep == .ShortBreakInProgress {
+                Text("Short Break \(pomodoroViewModel.displayTimeRemaining)")
+            } else if pomodoroViewModel.currentStep == .LongBreakNotStarted {
+                Text("Start Long Break")
+                Button(action: pomodoroViewModel.startLongBreak) {
+                    Text("Start")
+                }
+            } else if pomodoroViewModel.currentStep == .LongBreakInProgress {
+                Text("Long Break \(pomodoroViewModel.displayTimeRemaining)")
+            }
         }
         .padding()
     }
@@ -21,6 +41,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        ContentView(pomodoroViewModel: PomodoroViewModel())
     }
 }
