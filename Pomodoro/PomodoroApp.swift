9b2849e1a1f36022072282dc2aee3dfd6b44eacf
edwardkim12 <edward.kim@sibelhealth.com>
2023-05-25 20:51:11 -0500

    Initial Commit

---
+++ Pomodoro/PomodoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PomodoroApp.swift
+//  Pomodoro
+//
+//  Created by Edward Kim on 5/25/23.
+//
+
+import SwiftUI
+
+@main
+struct PomodoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
